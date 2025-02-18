0ce340fd1ff8627dffbed0ba4f34cea5ab45bb49
stoorps <stoorps@proton.me>
2025-02-18 12:34:15 +0000

    actually fixed post_update

--- build/filesystem/etc/post_update/post_update.sh
+++ build/filesystem/etc/post_update/post_update.sh
@@ -2,13 +2,13 @@
 set -oue pipefail
 
 # Check if the flag exists. If we've already deleted, then we've already ran.
-if [ ! -f /etc/post_update.flag ]; then
+if [ ! -f /etc/post_update/post_update.flag ]; then
     exit 0
 fi
 
 ./tmp/build/flatpaks.sh
 
 # Delete the flag. 
-rm -f /etc/post_update.flag 
+rm -f /etc/post_update/post_update.flag 
 
 exit 0
